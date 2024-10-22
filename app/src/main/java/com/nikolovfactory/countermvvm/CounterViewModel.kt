4a3b18feb4a796d32928c1f3461f50b9731a8a61
atanas-nikolov <master89q@gmail.com>
2024-10-22 18:34:49 +0300

    add MVVM class

---
+++ app/src/main/java/com/nikolovfactory/countermvvm/CounterViewModel.kt
@@ -0,0 +1,12 @@
+package com.nikolovfactory.countermvvm
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel(){
+    private val _count = mutableStateOf(0)
+
+    // Expose the count as an immutable state to accessed outside the class
+    val count : MutableState<Int> = _count
+}
