91088fa0b65c73010faaec32a3449a8a3a6cb295
atanas-nikolov <master89q@gmail.com>
2024-10-22 17:17:06 +0300

    add the Composable fun TheCounterApp with increment and decremen fun, Column and two Buttons

--- app/src/main/java/com/nikolovfactory/countermvvm/MainActivity.kt
+++ app/src/main/java/com/nikolovfactory/countermvvm/MainActivity.kt
@@ -3,13 +3,25 @@ package com.nikolovfactory.countermvvm
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.nikolovfactory.countermvvm.ui.theme.CounterMVVMTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,9 +34,45 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    TheCounterApp()
                 }
             }
         }
     }
 }
+
+@Composable
+fun TheCounterApp(){
+    val count = remember { mutableStateOf(0) }
+
+    fun increment() {
+        count.value++
+    }
+
+    fun decrement() {
+        count.value--
+    }
+
+    Column (modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+        ){
+        Text(text = "Count: ${count.value}",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Row {
+            Button(onClick = { increment() }) {
+                Text(text = "Increment")
+            }
+            Button(onClick = { decrement() }) {
+                Text(text = "Decrement")
+            }
+
+        }
+
+
+    }
+}
+
